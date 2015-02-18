a03ce2bcd7e582d0e02de150d72addea5a739e17
Rommel Rico <rommeltj@gmail.com>
2015-02-17 18:57:45 -0800

    Converting to a workspace and download pod
    Setting up LDProgressView

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, '8.0'
+pod 'LDProgressView', '~> 1.2'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// LDProgressView
+#define COCOAPODS_POD_AVAILABLE_LDProgressView
+#define COCOAPODS_VERSION_MAJOR_LDProgressView 1
+#define COCOAPODS_VERSION_MINOR_LDProgressView 2
+#define COCOAPODS_VERSION_PATCH_LDProgressView 1
+
