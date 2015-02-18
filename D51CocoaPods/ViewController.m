19fca0247f87c197c11a863b6b9b02a4e77febba
Rommel Rico <rommeltj@gmail.com>
2015-02-17 18:36:07 -0800

    Added a timer and downloadFile functionality

--- D51CocoaPods/ViewController.m
+++ D51CocoaPods/ViewController.m
@@ -8,9 +8,15 @@
 
 #import "ViewController.h"
 
+#define MAX_COUNT 10
+
 @interface ViewController ()
+
+@property int myCount;
+@property NSTimer *myTimer;
 @property (weak, nonatomic) IBOutlet UILabel *myLabel;
 @property (weak, nonatomic) IBOutlet UIProgressView *myProgressView;
+@property (weak, nonatomic) IBOutlet UIButton *myDownloadFileButton;
 
 @end
 
@@ -18,12 +24,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self updateDisplay];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)doDownloadFile:(id)sender {
+    self.myDownloadFileButton.enabled = NO;
+    self.myCount = 0;
+    [self updateDisplay];
+    self.myTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(doTimer) userInfo:nil repeats:YES];
+}
+
+- (void)doTimer {
+    self.myCount++;
+    [self updateDisplay];
+    if(self.myCount == MAX_COUNT) {
+        [self.myTimer invalidate]; //Stop the timer.
+        self.myDownloadFileButton.enabled = YES;
+    }
+}
+
+- (void)updateDisplay {
+    float progress = self.myCount / MAX_COUNT;
+    self.myLabel.text = [NSString stringWithFormat:@"%1.2f", progress];
+    self.myProgressView.progress = progress;
 }
 
 @end
